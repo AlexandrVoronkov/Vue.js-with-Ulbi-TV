806d9c8e94dadd2aae0d66ca4e78378a06b20293
Helvegen4 <def577g@gmail.com>
2022-08-09 16:53:28 +0700

    двухстороннее связывание input-ов body и title

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
-<div>
-  <div class="post">
-    <div><strong>Название:</strong> Пост о JS</div>
-    <div><strong>Описание:</strong> Не удалось загрузить карту исходного кода с помощью инструментов разработчика: Не удалось загрузить контент для chrome-extension://dipiagiiohfljcicegpgffpbnjmgjcnf/js/content.js.map: </div>
+<div class="app">
+  <form>
+    <h4>Создание поста</h4>
+    <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Название">
+    <input
+        v-bind:value="body"
+        @input="body = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Описание">
+    <button
+        class="btn"
+        @click="createPost">
+        Создать
+    </button>
+  </form>
+  <div class="post" v-for="post in posts" :key="post.id">
+    <div><strong>Название:</strong>{{ post.title}}</div>
+    <div><strong>Описание:</strong>{{ post.body}}</div>
   </div>
 </div>
 </template>
@@ -11,14 +31,20 @@
 export default {
  data(){
    return{
-     likes: 0,
+     posts : [
+       {id: 1, title: 'JavaScript 1', body: 'Описание поста 1'},
+       {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
+       {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
+       {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
+     ],
+     title: '',
+     body: ''
    }
  },
   methods: {
-   addLike(){
-     this.likes += 1;
-   }
-  }
+  createPost(){
+  },
+ }
 }
 </script>
 
@@ -29,10 +55,27 @@ export default {
   box-sizing: border-box;
 }
 
+.input {
+  width: 100%;
+  border: 1px solid rebeccapurple;
+  padding:  10px 15px;
+  margin-top: 15px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
 .post {
   padding: 15px;
   border: 2px solid rebeccapurple;
   margin-top: 10px;
 }
-
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: rebeccapurple;
+  border: 1px solid rebeccapurple;
+}
 </style>
