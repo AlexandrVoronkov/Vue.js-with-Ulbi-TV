c942b10517020ef534cb4632118b814d04679946
Helvegen4 <def577g@gmail.com>
2022-08-10 13:17:18 +0700

    два новых компонента: posform и postlist, первый отдает родителю данные, второй их отрисовывает. В posform создается событие create на которое подписываемся в app

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,19 @@
 <template>
 <div class="app">
-  <form @submit.prevent>
-    <h4>Создание поста</h4>
-    <input
-        v-bind:value="title"
-        @input="title = $event.target.value"
-        class="input"
-        type="text"
-        placeholder="Название">
-    <input
-        v-bind:value="body"
-        @input="body = $event.target.value"
-        class="input"
-        type="text"
-        placeholder="Описание">
-    <button
-        class="btn"
-        @click="createPost">
-        Создать
-    </button>
-  </form>
-  <div class="post" v-for="post in posts" :key="post.id">
-    <div><strong>Id:</strong>{{ post.id}}</div>
-    <div><strong>Название:</strong>{{ post.title}}</div>
-    <div><strong>Описание:</strong>{{ post.body}}</div>
-  </div>
+  <post-form
+    @create="createPost"
+  />
+  <post-list :posts="posts"/>
 </div>
 </template>
 
 <script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
 export default {
+  components: {
+    PostList, PostForm
+  },
  data(){
    return{
      posts : [
@@ -39,20 +23,11 @@ export default {
        {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
        {id: 5, title: 'JavaScript 5', body: 'Описание поста 5'},
      ],
-     title: '',
-     body: ''
    }
  },
   methods: {
-  createPost(){
-    const newPost = {
-      id: Date.now(),
-      title: this.title,
-      body: this.body,
-    }
-    this.posts.push(newPost);
-    this.title = '';
-    this.body = '';
+  createPost(post){
+  this.posts.push(post);
   },
  }
 }
@@ -65,27 +40,5 @@ export default {
   box-sizing: border-box;
 }
 
-.input {
-  width: 100%;
-  border: 1px solid rebeccapurple;
-  padding:  10px 15px;
-  margin-top: 15px;
-}
-form {
-  display: flex;
-  flex-direction: column;
-}
-.post {
-  padding: 15px;
-  border: 2px solid rebeccapurple;
-  margin-top: 10px;
-}
-.btn {
-  margin-top: 15px;
-  align-self: flex-end;
-  padding: 10px 15px;
-  background: none;
-  color: rebeccapurple;
-  border: 1px solid rebeccapurple;
-}
+
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,66 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input
+        v-model="post.title"
+        class="input"
+        type="text"
+        placeholder="Название">
+    <input
+        v-model="post.body"
+        class="input"
+        type="text"
+        placeholder="Описание">
+    <button
+        class="btn"
+        @click="createPost"
+    >
+      Создать
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      post: {
+        title: '',
+        body: ''
+      }
+    }
+  },
+  methods: {
+    createPost(){
+      this.post.id = Date.now();
+      this.$emit('create', this.post)
+      this.post = {
+        title: '',
+        body: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.input {
+  width: 100%;
+  border: 1px solid rebeccapurple;
+  padding:  10px 15px;
+  margin-top: 15px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: rebeccapurple;
+  border: 1px solid rebeccapurple;
+}
+</style>
