5f21ee3f3fda8df4bbe55ee95462c6ee5328ecf5
Helvegen4 <def577g@gmail.com>
2022-08-10 12:13:23 +0700

    добавление постов через форму

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="app">
-  <form>
+  <form @submit.prevent>
     <h4>Создание поста</h4>
     <input
         v-bind:value="title"
@@ -21,6 +21,7 @@
     </button>
   </form>
   <div class="post" v-for="post in posts" :key="post.id">
+    <div><strong>Id:</strong>{{ post.id}}</div>
     <div><strong>Название:</strong>{{ post.title}}</div>
     <div><strong>Описание:</strong>{{ post.body}}</div>
   </div>
@@ -36,6 +37,7 @@ export default {
        {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
        {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
        {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
+       {id: 5, title: 'JavaScript 5', body: 'Описание поста 5'},
      ],
      title: '',
      body: ''
@@ -43,6 +45,14 @@ export default {
  },
   methods: {
   createPost(){
+    const newPost = {
+      id: Date.now(),
+      title: this.title,
+      body: this.body,
+    }
+    this.posts.push(newPost);
+    this.title = '';
+    this.body = '';
   },
  }
 }
